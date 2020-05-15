0d20ec4acd0d58a08a2e976b012694ac08e1423c
adrii3 <aquinoa@elpuig.xeill.net>
2020-05-15 17:32:14 +0200

    tabaco

---
+++ app/src/main/java/com/example/puigincidencies/view/Profes/InicioProfesFragment.java
@@ -0,0 +1,33 @@
+package com.example.puigincidencies.view.Profes;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.puigincidencies.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InicioProfesFragment extends Fragment {
+
+
+    public InicioProfesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inicio_profes, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/puigincidencies/view/Profes/LoginProfesFragment.java
@@ -0,0 +1,105 @@
+package com.example.puigincidencies.view.Profes;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.puigincidencies.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
+
+public class LoginProfesFragment extends Fragment {
+
+
+    private EditText editEmailProfe, editContraseñaProfe;
+    private Button btnIniciarSesionProfe;
+    private ImageView imagenGoogle;
+    FirebaseAuth firebaseAuthProfe = FirebaseAuth.getInstance();
+    NavController navController;
+
+
+    public LoginProfesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_profes, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        editEmailProfe = view.findViewById(R.id.edit_text_usuario_profes);
+        editContraseñaProfe = view.findViewById(R.id.edit_text_contraseña_profes);
+
+        btnIniciarSesionProfe =view.findViewById(R.id.boton_login_iniciar_sesion_profes);
+        btnIniciarSesionProfe.setOnClickListener(new View.OnClickListener() {
+            //Inicio de  sesión de profes
+            @Override
+            public void onClick(View v) {
+                iniciarSesionProfes();
+            }
+        });
+
+        imagenGoogle =  view.findViewById(R.id.boton_iniciar_sesion_google);
+        imagenGoogle.setOnClickListener(new View.OnClickListener() {
+            //Inicio de sesión con cuenta de google
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void iniciarSesionProfes(){
+        String email = editEmailProfe.getText().toString();
+        String contraseña = editContraseñaProfe.getText().toString();
+
+        if(!email.isEmpty() && !contraseña.isEmpty()){
+            firebaseAuthProfe.signInWithEmailAndPassword(email,contraseña).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        actualizarProfeUI(firebaseAuthProfe.getCurrentUser());
+                    }else{
+                        Toast.makeText(getContext(),"Inicio de sesión incorrecto", LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }else{
+            Toast.makeText(getContext(),"Relllene los campos requeridos", Toast.LENGTH_LONG);
+            return;
+        }
+    }
+
+    private void actualizarProfeUI(FirebaseUser currentUser){
+        if(currentUser != null) {
+            navController.navigate(R.id.inicioProfesFragment);
+        }
+    }
+}
